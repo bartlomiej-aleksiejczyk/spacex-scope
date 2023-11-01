850cb3ae1a3dfd255755826b36073cd52bc074d0
bartlomiej-aleksiejczyk <bartlomiej.aleksiejczyk@gmail.com>
2023-11-01 17:24:36 +0100

    Extract single mission item from mission container

--- src/components/MissionsContainer.tsx
+++ src/components/MissionsContainer.tsx
@@ -1,19 +1,20 @@
 import { useQuery } from "@apollo/client";
 import { GET_MISSIONS } from "../graphql/getMissionsQuery";
 import { useState } from "react";
+import {MissionItem} from "./missionItem/MissionItem";
 
 export const MissionsContainer = () => {
 	const [missionLimit, setMissionLimit] = useState<number>(10);
 	const { loading, error, data } = useQuery(GET_MISSIONS, {
 		variables: { limit: missionLimit },
 	});
+
 	return (
 		<div>
 			{data?.launches.map((mission) => (
-				<div>
-					<span>{mission.mission_name}</span>
-				</div>
-			))}
+				<MissionItem mission={mission}/>
+				)
+				)}
 		</div>
 	);
 };
