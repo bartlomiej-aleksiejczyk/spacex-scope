939f61501f0a276c9d490ca583e5639d9a81b8c8
bartlomiej-aleksiejczyk <bartlomiej.aleksiejczyk@gmail.com>
2023-11-01 23:44:34 +0100

    Alter query to get single mission

--- src/graphql/queries/getSingleMissionQuery.ts
+++ src/graphql/queries/getSingleMissionQuery.ts
@@ -1,10 +1,10 @@
-import {gql} from "../generatedTypes";
+import gql from 'graphql-tag';
 
 export const GET_SINGLE_MISSION = gql`
 query GetSingleLaunch($launchId: ID!) {
   launch(id: $launchId) {
     details
-    launch_year
+    launch_date_local
     links {
       article_link
     }
