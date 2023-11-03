843f6d52a5de298cd7aa7e49066f6e92dc10ea77
bartlomiej-aleksiejczyk <bartlomiej.aleksiejczyk@gmail.com>
2023-11-03 23:01:49 +0100

    Change types in getLikedMissions.ts

--- src/utils/getLikedMissions.ts
+++ src/utils/getLikedMissions.ts
@@ -1,5 +1,5 @@
 export const getLikedMissions = () => {
-	const result = Object.keys(localStorage).reduce((accumulator, key) => {
+	const result = Object.keys(localStorage).reduce((accumulator: Record<unknown, unknown>, key) => {
 		accumulator[key] = localStorage.getItem(key);
 		return accumulator;
 	}, {});
